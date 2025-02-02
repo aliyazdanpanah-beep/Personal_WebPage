963bd8169003a7a1cd2a56b95d6121fbcca68338
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-02-02 22:28:04 +0330

    Update Home.jsx

--- Personal_WebPage/page/Home/Home.jsx
+++ Personal_WebPage/page/Home/Home.jsx
@@ -5,12 +5,22 @@ import "./Home.css";
 
 const Home = () => {
   const [nav, setNav] = useState([]);
+
   useEffect(() => {
+    // API call
+
     axios.get("http://localhost:8000/Nav").then((result) => {
       setNav(result.data.data);
     });
   }, []);
-  return <Nav />;
+
+  return (
+    <div className="Nav">
+      {nav.map((DataBase) => (
+        <Nav key={DataBase.id} data={DataBase} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
