812461774158576a466eaab160fb3a3e3d0c85eb
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-02-02 22:24:14 +0330

    Update Home.jsx

--- Personal_WebPage/page/Home/Home.jsx
+++ Personal_WebPage/page/Home/Home.jsx
@@ -1,10 +1,16 @@
-import Nav from "../../components/Nav/Nav"
-import "./Home.css"
+import Nav from "../../components/Nav/Nav";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./Home.css";
 
 const Home = () => {
-  return (
-    <Nav />
-  )
-}
+  const [nav, setNav] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8000/Nav").then((result) => {
+      setNav(result.data.data);
+    });
+  }, []);
+  return <Nav />;
+};
 
-export default Home
+export default Home;
