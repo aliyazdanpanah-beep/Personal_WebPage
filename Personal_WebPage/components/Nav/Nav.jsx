981ffce90033fa9cb9d1cfaa65ed8b5fee15a173
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-02-02 21:55:37 +0330

    crate Home page

---
+++ Personal_WebPage/components/Nav/Nav.jsx
@@ -0,0 +1,20 @@
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <div className="Nav_wrapper">
+      <div>
+        <span>Conect Me</span>
+      </div>
+
+      <div>
+        <span>Home</span>
+        <span>About</span>
+        <span>Progect</span>
+        <span>Linkdin</span>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
