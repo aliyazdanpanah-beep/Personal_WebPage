6b7e8dd3f584899ccfd5a25d7c7331e677a67cab
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-02-03 21:40:48 +0330

    add data to json server

--- Personal_WebPage/src/component/Section_1/Section1.jsx
+++ Personal_WebPage/src/component/Section_1/Section1.jsx
@@ -4,6 +4,10 @@ const Section1 = (props) => {
   return (
     <div className="Section1_Wrapper">
       <img src={props.data.imageURL} className="imageURL" />
+
+      <div className="sec1_text">
+          
+      </div>
     </div>
   );
 };
