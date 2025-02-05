9fa96d4f7303877d5b933696ceb2206b7bb2c19c
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-02-05 15:29:32 +0330

    add scroll animation for my project

--- Personal_WebPage/src/component/Section_2/Section_2.jsx
+++ Personal_WebPage/src/component/Section_2/Section_2.jsx
@@ -1,11 +1,14 @@
 import "./Section_2.css"
+import AOS from "aos"
+import "aos/dist/aos.css";
+
 
 const Project = (props) => {
   return (
     <div className="Project_Wrapper">
-      <div className="proj_item"> {props.data.text} </div>
-      <div className="proj_item"> {props.data.text} </div>
-      <div className="proj_item"> {props.data.text} </div>
+      <div data-aos="fade-right" className="proj_item"> {props.data.text} </div>
+      <div data-aos="fade-right" className="proj_item"> {props.data.text} </div>
+      <div data-aos="fade-right" className="proj_item"> {props.data.text} </div>
       <div className="proj_item"> {props.data.text} </div>
       <div className="proj_item"> {props.data.text} </div>
       <div className="proj_item"> {props.data.text} </div>
