5ad02a17f4ca01eb6e83f66759b8563df7472415
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-02-05 13:42:06 +0330

    add scroll animation

--- Personal_WebPage/src/component/Section_1/Section1.jsx
+++ Personal_WebPage/src/component/Section_1/Section1.jsx
@@ -1,14 +1,28 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./Section1.css";
 
 const Section1 = (props) => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+    });
+  }, []);
   return (
-    <div className="Section1_Wrapper">
-      <img src={props.data.imageURL} className="imageURL" />
+    <div>
+      <div className="Section1_Wrapper">
+        <img
+          src={props.data.imageURL}
+          className="imageURL"
+          data-aos="fade-down-right"
+        />
 
-      <div className="sec1_text">
-          <p> {props.data.text1} </p>
-          <p> {props.data.text2} </p>
-          <p> {props.data.text3} </p>
+        <div className="sec1_text">
+          <p data-aos="fade-left"> {props.data.text1} </p>
+          <p data-aos="fade-left"> {props.data.text2} </p>
+          <p data-aos="fade-left"> {props.data.text3} </p>
+        </div>
       </div>
     </div>
   );
